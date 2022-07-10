6bc2443f59df063968d3a7cf046fe32711a5ae18
Alexman604 <goa-man-604@ukr.net>
2022-07-10 21:47:15 +0300

    card

---
+++ card-component.js
@@ -0,0 +1,43 @@
+import {Component, ReactNode} from "react";
+
+const CardComponent = () => {
+
+return (
+    <div className="wrapper"> 
+     <div  className="picture" > <img src = "{picture_link} alt = ""/> </div>> 
+    <div class = "raiting-price" >
+        <
+        div class = "raiting" > $ {
+            this.state.rating
+        } < i class = "bi bi-star" > < /i> </div >
+        <
+        div class = "price" > $ {
+            this.state.price
+        } < /div> <
+        /div> <
+        div class = "item-title" > $ {
+            this.state.title
+        } < /div> <
+        div class = "type" > $ {
+            this.state.category
+        } < /div> 
+        <div class = "button" > ADD TO CART < /div></div>
+    
+    
+    </div>
+    
+    
+    
+    
+    
+    
+   
+
+
+    )
+
+
+
+}
+
+export default CardComponent;
